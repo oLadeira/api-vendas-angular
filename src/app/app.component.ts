6c9ebaa5fd4a903e5e5f99379b0de566de99b318
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-10 20:56:08 -0300

    :sparkles: feature: Toggle sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'cliente-app';
+
+  ngAfterViewInit(): void {
+
+    var path = location.pathname;
+
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index:any, element:any) => {
+        if ($(element).attr('href') === path) {
+            $(element).addClass("active");
+        }
+    });
+
+    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+
+    /*
+      (function($) {
+        "use strict";
+
+        // Add active state to sidbar nav links
+        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function(this) {
+                if (this.getAttribute("href") === path) {
+                    $(this).addClass("active");
+                }
+            });
+
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function(e) {
+            e.preventDefault();
+            $("body").toggleClass("sb-sidenav-toggled");
+        });
+    })(jQuery);(function($) {
+      "use strict";
+
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function(this) {
+              if (this.getAttribute("href") === path) {
+                  $(this).addClass("active");
+              }
+          });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+*/
+
+  }
 }
