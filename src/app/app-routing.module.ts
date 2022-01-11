d0c68df59c623fc0df965bdd75375594bfe406a5
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-11 12:43:27 -0300

    :art: visual: configurando navegacao atraves de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ClientesFormComponent } from './clientes/clientes-form/clientes-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'clientes', component: ClientesFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
