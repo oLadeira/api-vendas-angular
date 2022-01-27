05bad31f150df5c03dc56b078e733a431be0f5bb
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-27 13:11:54 -0300

    :art: visual: trabalhando com Rotas Filhas

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -56,7 +56,7 @@ export class ClientesFormComponent implements OnInit {
   }
 
   voltarParaListagem(){
-    this.router.navigate(["/clientes-lista"])
+    this.router.navigate(["/clientes/lista"])
   }
 
 }
