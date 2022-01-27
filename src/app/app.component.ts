f617e8c70f033d4d372faa5d01ac98def9cdaa0e
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-27 11:41:17 -0300

    :art: visual: separando o Login do Template da aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import * as jQuery from 'jquery';
 
@@ -7,58 +7,11 @@ import * as jQuery from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'cliente-app';
 
-  ngAfterViewInit(): void {
 
-    var path = location.pathname;
-
-    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index:any, element:any) => {
-        if ($(element).attr('href') === path) {
-            $(element).addClass("active");
-        }
-    });
-
-    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-
-    /*
-      (function($) {
-        "use strict";
-
-        // Add active state to sidbar nav links
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function(this) {
-                if (this.getAttribute("href") === path) {
-                    $(this).addClass("active");
-                }
-            });
-
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
-        });
-    })(jQuery);(function($) {
-      "use strict";
-
-      // Add active state to sidbar nav links
-      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function(this) {
-              if (this.getAttribute("href") === path) {
-                  $(this).addClass("active");
-              }
-          });
-
-      // Toggle the side navigation
-      $("#sidebarToggle").on("click", function(e) {
-          e.preventDefault();
-          $("body").toggleClass("sb-sidenav-toggled");
-      });
-  })(jQuery);
-*/
-
-  }
 }
